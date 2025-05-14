44b1d6ad497f1f22a0617fe7d449714b83095a32
hkudarha <hkudarha@gmail.com>
2025-05-14 17:07:17 +0530

    again push

--- src/constants/content/MainContent.js
+++ src/constants/content/MainContent.js
@@ -20,7 +20,6 @@ export const backendConfig = {
   // origin: "https://api.bitblock.network",
   // base: "http://192.168.3.56:6018/api",
   // origin: "http://192.168.3.56:6018",
-<<<<<<< HEAD
   // base: "https://api.worldtrades.in/api",
   // origin: "https://api.worldtrades.in",
   // base: "http://192.168.29.127:6042/api",
